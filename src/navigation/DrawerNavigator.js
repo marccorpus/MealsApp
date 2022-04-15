5bc1a916dba46d7f2b17532db3a73b1fb0b43037
marccorpus <marccorpus.dev@gmail.com>
2022-04-15 23:28:53 +0800

    Drawer and Header styles

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,22 +1,47 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeScreen from "../screens/HomeScreen";
 import FavoritesScreen from "../screens/FavoritesScreen";
+
+import colors from "../utils/colors";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: { backgroundColor: colors.brown600 },
+        drawerActiveBackgroundColor: colors.brown900,
+        drawerActiveTintColor: colors.white,
+        drawerInactiveTintColor: colors.black,
+        headerStyle: {
+          backgroundColor: colors.brown900,
+        },
+        headerTintColor: colors.white,
+      }}
+    >
       <Drawer.Screen
         name="homeDrawer"
         component={HomeScreen}
-        options={{ title: "All Categories", drawerLabel: "Home" }}
+        options={{
+          title: "All Categories",
+          drawerLabel: "Home",
+          drawerIcon: ({ color, size }) => {
+            return <Ionicons name="home" color={color} size={size} />;
+          },
+        }}
       />
       <Drawer.Screen
         name="favoritesDrawer"
         component={FavoritesScreen}
-        options={{ title: "Favorites" }}
+        options={{
+          title: "Favorites",
+          drawerIcon: ({ color, size }) => {
+            return <Ionicons name="list" color={color} size={size} />;
+          },
+        }}
       />
     </Drawer.Navigator>
   );
---
+++ src/utils/colors.js
@@ -0,0 +1,8 @@
+export default {
+  black: "#050505",
+  white: "#fff",
+  brown600: "#FFF3E4",
+  brown700: "#EED6C4",
+  brown800: "#6B4F4F",
+  brown900: "#483434",
+};
